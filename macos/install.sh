445640314b6bfd29e35a53392fccfb4d5b599749
Sam Parkin <sam.parkin@gmail.com>
2017-06-07 12:06:18 +1200

    Prevent annoying popups when attaching devices

--- macos/install.sh
+++ macos/install.sh
@@ -7,6 +7,12 @@ osascript -e 'tell application "System Preferences" to quit'
 # Disable "natural" scrolling
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Prevent Photos from opening automatically when devices are plugged in
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
 for app in "Activity Monitor" \
 	"cfprefsd" \
 	"Finder"; do
